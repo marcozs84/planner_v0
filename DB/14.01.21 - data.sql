b18371cd99867e9631c150f74c6b44d73fe44eb8
Marco Zeballos <marcozs84@gmail.com>
2014-01-21 13:40:26 -0400

    DB: new field: tblSplit.status, new scripts for schema and data 14.01.21_

---
+++ DB/14.01.21 - data.sql
@@ -0,0 +1,80 @@
+﻿# SQL Manager 2010 for MySQL 4.5.0.9
+# ---------------------------------------
+# Host     : localhost
+# Port     : 3306
+# Database : weekplanner
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+SET FOREIGN_KEY_CHECKS=0;
+
+USE weekplanner;
+
+#
+# Data for the `tblproject` table  (LIMIT 0,500)
+#
+
+INSERT INTO `tblproject` (id, name, description, startDate, endDate) VALUES 
+  (1,'Work Plan 2014','Test Work Plan for 2014','2014-01-06','2014-12-26');
+COMMIT;
+
+#
+# Data for the `tblresource` table  (LIMIT 0,500)
+#
+
+INSERT INTO `tblresource` (id, name, initials, status) VALUES 
+  (1,'Marco','MZ',1),
+  (2,'Vivian','VT',1),
+  (3,'Victor','VV',1),
+  (4,'Limberg','LB',1),
+  (5,'David','DS',1),
+  (6,'Luis','LV',1);
+COMMIT;
+
+#
+# Data for the `tblsplit` table  (LIMIT 0,500)
+#
+
+INSERT INTO `tblsplit` (id, parentId, timelineId, dayId, assigned, closed, startDate, originalDate, delayBeginning, delay, duration, sorting, status) VALUES 
+  (1,1,6,0,1,0,'0000-00-00 00:00:00','0000-00-00 00:00:00',0,0,5,1,0),
+  (2,1,3,0,1,0,'0000-00-00 00:00:00','0000-00-00 00:00:00',0,0,4,1,0),
+  (3,1,2,0,1,0,'0000-00-00 00:00:00','0000-00-00 00:00:00',0,0,4,1,0),
+  (4,1,5,0,1,0,'0000-00-00 00:00:00','0000-00-00 00:00:00',0,0,7,1,0),
+  (5,2,2,0,1,0,'0000-00-00 00:00:00','0000-00-00 00:00:00',0,0,2,2,0),
+  (6,3,6,0,1,0,'0000-00-00 00:00:00','0000-00-00 00:00:00',0,0,12,2,0),
+  (7,3,4,0,1,0,'0000-00-00 00:00:00','0000-00-00 00:00:00',0,0,28,1,0),
+  (8,3,6,0,1,0,'0000-00-00 00:00:00','0000-00-00 00:00:00',0,0,4,3,0);
+COMMIT;
+
+#
+# Data for the `tbltask` table  (LIMIT 0,500)
+#
+
+INSERT INTO `tbltask` (id, name, description, duration, assigned, closed, color) VALUES 
+  (1,'Estimation Tickets release 2.29','All team working to estimage tickets for release 2.29',20,0,0,'FFCC00'),
+  (2,'Work Plan Planning/Updates','Time spent planning or updating the excel sheet.',2,0,0,'009BC3'),
+  (3,'Invoices Scan','Time spent creating parsers for Invoices',44,0,0,'0059B1');
+COMMIT;
+
+#
+# Data for the `tbltimeline` table  (LIMIT 0,500)
+#
+
+INSERT INTO `tbltimeline` (id, name, teamId, projectId, resourceId) VALUES 
+  (1,NULL,0,1,1),
+  (2,NULL,0,1,2),
+  (3,NULL,0,1,3),
+  (4,NULL,0,1,4),
+  (5,NULL,0,1,5),
+  (6,NULL,0,1,6);
+COMMIT;
+
+
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ DB/14.01.21 - schema.sql
@@ -0,0 +1,147 @@
+﻿# SQL Manager 2010 for MySQL 4.5.0.9
+# ---------------------------------------
+# Host     : localhost
+# Port     : 3306
+# Database : weekplanner
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+SET FOREIGN_KEY_CHECKS=0;
+
+DROP DATABASE IF EXISTS weekplanner;
+
+CREATE DATABASE weekplanner
+    CHARACTER SET 'utf8'
+    COLLATE 'utf8_general_ci';
+
+USE weekplanner;
+
+#
+# Structure for the `tblday` table : 
+#
+
+DROP TABLE IF EXISTS tblday;
+
+CREATE TABLE tblday (
+  dayId int(11) NOT NULL AUTO_INCREMENT,
+  timelineId int(11) DEFAULT NULL,
+  date datetime DEFAULT NULL,
+  week int(11) DEFAULT NULL,
+  day int(11) DEFAULT NULL,
+  totalHours int(11) DEFAULT NULL,
+  used int(11) DEFAULT NULL,
+  PRIMARY KEY (dayId),
+  UNIQUE KEY dayId (dayId)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+#
+# Structure for the `tblproject` table : 
+#
+
+DROP TABLE IF EXISTS tblproject;
+
+CREATE TABLE tblproject (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  name varchar(250) DEFAULT NULL,
+  description text,
+  startDate date DEFAULT '0000-00-00',
+  endDate date DEFAULT '0000-00-00',
+  PRIMARY KEY (id),
+  UNIQUE KEY id (id)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+#
+# Structure for the `tblresource` table : 
+#
+
+DROP TABLE IF EXISTS tblresource;
+
+CREATE TABLE tblresource (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  name varchar(250) NOT NULL,
+  initials varchar(20) DEFAULT NULL,
+  status int(11) DEFAULT '1',
+  PRIMARY KEY (id),
+  UNIQUE KEY id (id)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+
+#
+# Structure for the `tblsplit` table : 
+#
+
+DROP TABLE IF EXISTS tblsplit;
+
+CREATE TABLE tblsplit (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  parentId int(11) DEFAULT '0',
+  timelineId int(11) DEFAULT '0',
+  dayId int(11) DEFAULT '0',
+  assigned int(11) DEFAULT '0',
+  closed int(11) DEFAULT '0',
+  startDate datetime DEFAULT NULL,
+  originalDate datetime DEFAULT NULL,
+  delayBeginning int(11) DEFAULT '0',
+  delay int(11) DEFAULT '0',
+  duration int(11) DEFAULT '0',
+  sorting int(11) DEFAULT '0',
+  status int(11) DEFAULT '0',
+  PRIMARY KEY (id),
+  UNIQUE KEY id (id)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+
+#
+# Structure for the `tbltask` table : 
+#
+
+DROP TABLE IF EXISTS tbltask;
+
+CREATE TABLE tbltask (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  name varchar(250) DEFAULT NULL,
+  description text,
+  duration int(11) DEFAULT '0',
+  assigned int(11) DEFAULT '0',
+  closed int(11) DEFAULT '0',
+  color varchar(20) DEFAULT NULL,
+  PRIMARY KEY (id),
+  UNIQUE KEY id (id)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+#
+# Structure for the `tblteam` table : 
+#
+
+DROP TABLE IF EXISTS tblteam;
+
+CREATE TABLE tblteam (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  name varchar(250) DEFAULT NULL,
+  PRIMARY KEY (id),
+  UNIQUE KEY id (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+#
+# Structure for the `tbltimeline` table : 
+#
+
+DROP TABLE IF EXISTS tbltimeline;
+
+CREATE TABLE tbltimeline (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  name text,
+  teamId int(11) DEFAULT '0',
+  projectId int(11) DEFAULT '0',
+  resourceId int(11) DEFAULT '0',
+  PRIMARY KEY (id),
+  UNIQUE KEY id (id)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+
+
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
